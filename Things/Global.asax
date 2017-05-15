ee45b5b97fc540151777a19c78980d8ce2e70b98
Fredrik K <fredrik.k@protonmail.ch>
2017-05-15 23:17:10 +0200

    Add project files.

---
+++ Things/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Things.MvcApplication" Language="C#" %>
---
+++ Things/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Things.Startup))]
+namespace Things
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
